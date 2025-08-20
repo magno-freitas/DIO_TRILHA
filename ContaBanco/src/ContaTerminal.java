e329063a54d4bfdca2435b7cb9a6f5b9d8586604
magno-freitas <magnovaladarescontato@gmail.com>
2025-08-20 14:19:59 -0300

    dio

---
+++ ContaBanco/src/ContaTerminal.java
@@ -0,0 +1,25 @@
+
+import java.util.Scanner;
+
+public class ContaTerminal {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int numero;
+        String agencia;
+        String nomeCliente;
+        float saldo;
+
+        System.out.println("Informe o número da conta: ");
+        numero = sc.nextInt();
+        sc.nextLine(); // Consume newline
+        System.out.println("Informe o número da agência: ");
+        agencia = sc.nextLine();
+        System.out.println("Informe o nome do cliente: ");
+        nomeCliente = sc.nextLine();
+        System.out.println("Informe o saldo: ");
+        saldo = sc.nextFloat();
+        
+        System.out.println("Olá "+ nomeCliente+ " obrigado por criar uma conta em nosso banco, sua agência é "+agencia+" conta "+numero+" e seu saldo "+saldo+" já está disponível para saque");
+    }
+}
